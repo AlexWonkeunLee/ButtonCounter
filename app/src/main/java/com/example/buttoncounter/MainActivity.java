b93fc6c3ada81e9237f30dfb487355bd22caec57
AlexWonkeunLee <AlexWonkeunLee@gmail.com>
2020-08-10 21:28:10 -0500

    basic onclicklistener function

--- app/src/main/java/com/example/buttoncounter/MainActivity.java
+++ app/src/main/java/com/example/buttoncounter/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.buttoncounter;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText userInput;
+    private Button button;
+    private TextView textView;
+    private int numTimesClicked = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userInput = (EditText) findViewById(R.id.editText);
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+        View.OnClickListener ourOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numTimesClicked++;
+                String result = "\nThe button got tapped " + numTimesClicked + " time(s)";
+                textView.append(result);
+
+            }
+        };
+        button.setOnClickListener(ourOnClickListener);
     }
 }
